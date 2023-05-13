f1cfbafc5e0c4e3262a42fba026abae4cd18fbdd
EmiAr5 <earduzzo@gmail.com>
2023-05-13 04:24:39 -0300

    Api Proyectos
    Creación de modelo y servicio, implementación en componente Proyecto.

--- src/app/components/proyecto/proyecto.component.ts
+++ src/app/components/proyecto/proyecto.component.ts
@@ -1,10 +1,40 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Proyecto } from 'src/app/model/proyecto';
+import { ProyectoService } from 'src/app/service/proyecto.service';
 
 @Component({
   selector: 'app-proyecto',
   templateUrl: './proyecto.component.html',
   styleUrls: ['./proyecto.component.css']
 })
-export class ProyectoComponent {
+export class ProyectoComponent implements OnInit {
 
+  proyecto: Proyecto[] = [];
+
+  constructor(private proyectoS: ProyectoService) { }
+  
+  ngOnInit(): void {
+    this.cargarProyecto();
+  }
+
+  cargarProyecto(): void{
+    this.proyectoS.list().subscribe(
+      data => {
+        this.proyecto = data;
+      }
+    )
+  }
+
+  deleteProyecto(id?: number): void{
+    if (id != undefined) {
+      this.proyectoS.delete(id).subscribe(
+        data => {
+          this.cargarProyecto();
+        }, err => {
+          alert("No se pudo eliminar el proyecto");
+        }
+      )
+    }
+  }
+  
 }
